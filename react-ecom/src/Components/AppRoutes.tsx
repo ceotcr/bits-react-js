056be10318987977a88614d5e55f11c1ce98bd80
Chetan (TCR) Sapkal <68210482+ceotcr@users.noreply.github.com>
2025-02-25 18:21:01 +0530

    Update document title and refactor navigation links to use NavLink for active styling

--- react-ecom/src/Components/AppRoutes.tsx
+++ react-ecom/src/Components/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router"
+import { Routes, Route, useLocation } from "react-router"
 import MainLayout from "./MainLayout"
 import HomePage from "../pages/HomePage"
 import ProductsPage from "../pages/ProductsPage"
@@ -7,8 +7,23 @@ import NotFound from "../pages/NotFound"
 import CartPage from "../pages/CartPage"
 import AuthPage from "../pages/AuthPage"
 import AdminPage from "../pages/AdminPage"
+import { useEffect } from "react"
+
+
+const docTitles = {
+    '/': 'Home | Opulenze',
+    '/products': 'Products | Opulenze',
+    '/cart': 'Cart | Opulenze',
+    '/login': 'Login | Opulenze',
+    '/admin': 'Admin | Opulenze',
+}
+
 
 const AppRoutes = () => {
+    const location = useLocation()
+    useEffect(() => {
+        document.title = docTitles[location.pathname as keyof typeof docTitles] || 'Opulenze'
+    }, [location])
     return (
         <Routes>
             <Route path="/" element={<MainLayout />}>
--- react-ecom/src/Components/layout/Header.tsx
+++ react-ecom/src/Components/layout/Header.tsx
@@ -1,5 +1,5 @@
 import { LiaShoppingBagSolid } from "react-icons/lia"
-import { Link, useParams } from "react-router"
+import { NavLink, useParams } from "react-router"
 import { useCart } from "../../contexts/CartContext"
 import { useAuth } from "../../contexts/AuthContext"
 import { useEffect, useState } from "react"
@@ -20,23 +20,23 @@ const Header = () => {
                 <nav className="flex justify-between items-center w-full max-w-[1440px] mx-auto p-4">
                     <h2 className="text-2xl font-semibold">Opulenze</h2>
                     <ul className="hidden items-center gap-4 text-sm font-medium text-gray-900 lg:flex">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/products">Products</Link></li>
+                        <li><NavLink className={({ isActive }) => isActive ? "underline underline-offset-2" : ""} to="/">Home</NavLink></li>
+                        <li><NavLink className={({ isActive }) => isActive ? "underline underline-offset-2" : ""} to="/products">Products</NavLink></li>
                         {
-                            isAuthenticated && <li><Link to="/admin">Admin</Link></li>
+                            isAuthenticated && <li><NavLink className={({ isActive }) => isActive ? "underline underline-offset-2" : ""} to="/admin">Admin</NavLink></li>
                         }
                         <li>
                             {
-                                !isAuthenticated ? <Link to="/login">Login</Link> :
+                                !isAuthenticated ? <NavLink className={({ isActive }) => isActive ? "underline underline-offset-2" : ""} to="/login">Login</NavLink> :
                                     <button className="cursor-pointer" onClick={logout}>Logout</button>
                             }
                         </li>
                     </ul>
                     <div className="flex items-center gap-4">
-                        <Link to={'/cart'} className="relative">
+                        <NavLink className={({ isActive }) => isActive ? "underline relative underline-offset-2" : "relative"} to={'/cart'}>
                             <LiaShoppingBagSolid size={24} />
                             <span className="absolute -top-2 -right-2 bg-green-500 text-white rounded-full w-5 h-5 text-xs flex justify-center items-center">{quantity}</span>
-                        </Link>
+                        </NavLink>
                         <button className="lg:hidden cursor-pointer" onClick={() => setMobileMenu(!mobileMenu)}>
                             {mobileMenu ? <MdClose size={28} /> : <MdMenu size={28} />}
                         </button>
@@ -46,13 +46,13 @@ const Header = () => {
             {
                 mobileMenu && (
                     <nav className="flex flex-col gap-4 font-medium text-gray-800 lg:hidden absolute top-20 text-xl right-4 py-4 rounded bg-[rgba(255,255,255,0.6)] backdrop-blur-lg shadow-2xl">
-                        <Link className="px-8" to="/">Home</Link>
-                        <Link className="px-8" to="/products">Products</Link>
+                        <NavLink className={({ isActive }) => isActive ? "px-8 underline underline-offset-2" : "px-8"} to="/">Home</NavLink>
+                        <NavLink className={({ isActive }) => isActive ? "px-8 underline underline-offset-2" : "px-8"} to="/products">Products</NavLink>
                         {
-                            isAuthenticated && <Link className="px-8" to="/admin">Admin</Link>
+                            isAuthenticated && <NavLink className={({ isActive }) => isActive ? "px-8 underline underline-offset-2" : "px-8"} to="/admin">Admin</NavLink>
                         }
                         {
-                            !isAuthenticated ? <Link className="px-8" to="/login">Login</Link> :
+                            !isAuthenticated ? <NavLink className={({ isActive }) => isActive ? "px-8 underline underline-offset-2" : "px-8"} to="/login">Login</NavLink> :
                                 <button className="cursor-pointer px-8 text-left" onClick={logout}>Logout</button>
                         }
 
