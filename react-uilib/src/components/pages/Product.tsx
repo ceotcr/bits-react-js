2fa48cb1515aab991e0c18bc2639f93de9a6b66f
Chetan (TCR) Sapkal <68210482+ceotcr@users.noreply.github.com>
2025-02-28 09:35:24 +0530

    Add "Add to cart" button functionality in Product component

--- react-uilib/src/components/pages/Product.tsx
+++ react-uilib/src/components/pages/Product.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import { useProduct } from "../../lib/contexts/ProductsContext.tsx";
 import { Link, useNavigate, useParams } from "react-router";
-import { Pen, Star, Trash } from "lucide-react";
+import { Pen, ShoppingBag, Star, Trash } from "lucide-react";
 import { Button } from "../ui/button";
 import { useQuery } from "@tanstack/react-query";
 import { getProduct } from "../../lib/APICalls/products.ts";
 import ConfirmDeleteDialog from "../custom/products/ConfirmDeleteDialog";
+import { useCartStore } from "@/store/cartStore.ts";
 
 const Product = () => {
     const { id } = useParams<{ id: string }>();
     const { removeProduct } = useProduct();
+    const { addToCart } = useCartStore();
     const navigate = useNavigate();
 
     const { data: product, isLoading: loading, error } = useQuery({
@@ -40,7 +42,7 @@ const Product = () => {
                 <p className="text-lg flex gap-4"><Star fill="yellow" size={24} /> {product.rating.rate} ({product.rating.count})</p>
                 <p className="text-lg">{product.description}</p>
                 <p className="text-lg font-bold">${product.price}</p>
-                <div className="flex gap-4">
+                <div className="flex gap-4 items-end">
                     <Link to={`/products/${product.id}/edit`} className="bg-blue-500 hover:bg-blue-600 cursor-pointer w-12 h-12 flex rounded-lg items-center justify-center">
                         <Pen size={24} fill="white" stroke="transparent" />
                     </Link>
@@ -49,6 +51,12 @@ const Product = () => {
                         onClick={() => setConfirmDelete(true)}
                     >
                         <Trash size={24} fill="white" />
+                    </Button>
+                    <Button
+                        className="bg-blue-500 hover:bg-blue-600 w-12 h-12 cursor-pointer"
+                        onClick={() => addToCart(product)}
+                    >
+                        <ShoppingBag size={24} />
                     </Button>
                 </div>
             </div>
